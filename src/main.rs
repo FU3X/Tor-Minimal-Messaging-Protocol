d719bd27aff0e6ce02fc581aa087fd414af7d129
John Stoober <JohnStoober@github.noreply.com>
2022-11-17 11:03:22 -0600

    added some useless stuff

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::Write;
 fn main() {
     println!("Welcome to TMMP(\"Tor Minimal Messaging Protocol\"), run /help");
     let mut username = String::new(); //note to self: leave this variable outside f the loop in
-                                      //order to keep it from resetting every time
+                                  //order to keep it from resetting every time
     
 
 
@@ -14,27 +14,28 @@ fn main() {
         
         
         if  x.0== 0 {
-            println!("[system]$ ERROR: Invalid Input! Try again");
+            println!("[system]$ ERROR: Invalid Input! Try again\n");
         }
 
         if x.0 == 1 {
-            println!("Exitting, Goodbye!");
+            println!("Exitting, Goodbye!\n");
             break;
         }
 
         if x.0 == 2 {
-            println!("Help: \n Run /username to change your username");
-            println!("Run /exit to exit the program");
-            println!("Run /join to join a server");
-            println!("Run /leave to leave a server");
-            println!("Run /server_run to run a server(you will be unable to join a server until you stop yours)");
+            println!("Help: \nRun /username to change your username\n");
+            println!("Run /exit to exit the program\n");
+            println!("Run /join to join a server\n");
+            println!("Run /leave to leave a server\n");
+            println!("Run /server_run to run a server(you will be unable to join a server until you stop yours)\n");
+            println!("Run /msg to send a message\n");
         }
 
         if x.0 == 3{
             username = x.1;
         }
 
-        println!("The username is {}",username);     
+        println!("The username is {}\n",username);     
     }
 
 
@@ -48,12 +49,17 @@ fn get_input() -> (u32, String) {
     let mut input1 = String::new();
     io::stdin().read_line(&mut input1)
         .expect("Error getting inputt");
-    
+    println!();
+
     let len = input1.len();
     let mut slice1 = String::from("Empty");
     let mut slice2 = String::from("Empty");
 
     if len > 9 {
+        if len > 25 {
+            println!("$ ERROR: Too Many Characters!\n");
+            return(0,input1);
+        }
         slice1 = input1[..9].to_string();
         slice2 = input1[9..len].to_string();
     }
