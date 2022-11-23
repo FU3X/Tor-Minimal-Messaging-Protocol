3346bd172f56da00eab189ce01628238e6873a3a
John Stoober <JohnStoober@github.noreply.com>
2022-11-23 15:09:09 -0600

    shortened lots of things

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::io;
 use std::io::Write;
 use libtor::{Tor, TorFlag, TorAddress, HiddenServiceVersion};
 use std::process::Command;
+use std::iter;
 
 fn main(){
     
     println!("Welcome to TMMP(\"Tor Minimal Messaging Protocol\"), run /help");
     let mut username = String::new(); 
-    let mut port: u16;
+    let mut port1: u16;
+    let mut port2: u16;
      
     loop{
 
@@ -28,7 +30,8 @@ fn main(){
             println!("Run /exit to exit the program\n");
             println!("Run /join to join a server\n");
             println!("Run /leave to leave a server\n");
-            println!("Run /server (port) to run a server(you will be unable to join a server until you stop yours)\n");
+            println!("Run \"/server (port_for_hidden_service)\" To run a server(you will be unable to join a server 
+            until you stop yours). Max port number is 65,535 use no commas.\n");
             println!("Run /msg to send a message\n");
             
         }
@@ -42,8 +45,10 @@ fn main(){
         if x.0 == 4{
 
             println!("Creating server...");
-            port = x.2;
-            server(port);
+            port1 = x.2;
+            port2 = x.3;
+            println!("port1: {} port2: {}",port1,port2);
+
             println!("\nstopping server\n");
 
         }
@@ -54,7 +59,7 @@ fn main(){
     return;
 }
 
-fn get_input() -> (u32, String, u16) {
+fn get_input() -> (u32, String, u16, u16) {
     print!("$ ");
 
     io::stdout()
@@ -69,64 +74,50 @@ fn get_input() -> (u32, String, u16) {
 
     println!();
 
-    let len = input1.len();
-    let mut slice1 = String::from("Empty");
-    let mut slice2 = String::from("Empty");
-    let mut slice3 = String::from("Empty");
-    let mut _slice4 = String::from("Empty");
+
+    let v: Vec<&str> = input1.split(' ').collect();
+
     
-    let mut port : &str = "empty";
-    if len > 9 {
+    if v[0].trim() == "/exit" {
 
-        if len > 35 {
-
-            println!("$ ERROR: Too Many Characters!\n");
-            return(0,input1,0);
-        }
-
-        slice1 = input1[..9].to_string();
-        slice2 = input1[9..len].to_string();
-        slice3 = input1[..7].to_string();
-        _slice4 = input1[7..len].to_string();
-        port = _slice4.trim();
-
-        
-    }
-
-    if input1.trim() == "/exit" {
-
-        return (1,input1,0);
+        return (1,v[0].to_string(),0,0);
 
     }
 
-    if input1.trim() == "/help" {
+    if v[0].trim() == "/help" {
 
-        return (2,input1,0);
-
-    }
-
-    if slice1.trim() == "/username" {
-
-        return (3,slice2,0);
+        return (2,v[0].to_string(),0,0);
 
     }
 
-    if slice3.trim() == "/server" {
-        println!("{}",slice3.trim());
-        match port.parse::<u16>() {
-            Ok(..) => return (4, input1, port.parse::<u16>().unwrap()),
-            Err(..) => println!("[system] ERROR! Invalid port!\n"),
+    if v[0].trim() == "/username" {
+
+        return (3,v[1].to_string(),0,0);
+
+    }
+
+    if v[0].trim() == "/server" {
+        println!("{}",v[0].trim());
+        match v[1].parse::<u16>() {
+            Ok(..) => return(4,v[0].to_string(),v[1]
+            .parse::<u16>()
+            .unwrap(),v[2]
+            .parse::<u16>()
+            .unwrap()),
+
+            Err(..) => {println!("Not ok")},
         };
+        
 
-        return (0,slice2,0);
+        return (0,v[0].to_string(),0,0);
 
     }
 
-    return (0,input1,0);
+    return (0,v[0].to_string(),0,0);
 
 }
 
-fn server(port: u16) {
+/* fn server(port: u16) {
   
     println!("\n[system]$ starting server on port {}",port);
     command("rm -rf /tmp/tor-rust");
@@ -136,16 +127,16 @@ fn server(port: u16) {
             .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
             .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
             .flag(TorFlag::HiddenServicePort(TorAddress::Port(port),None.into()))
-            .start(){
-                        Ok(..) => println!("Did it"),
-                        Err(..) => println!("couldnt do it"),
-
+            .start() {
+                Ok(..) => println!("Finished"),
+                Err(..) => println!("Couldnt do it"),
             };
+
     println!("[system]$ Finished initializing tor daemon\nTor sock running on port 19050\nTor Hidden Service running on port {}",port);
 
 
     return;
-}
+} */
 
 fn command(stir: &str) {
     Command::new("sh")
